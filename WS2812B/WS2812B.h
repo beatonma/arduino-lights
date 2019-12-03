43fc89df2944a3f5f770efcf619679291f15c906
Michael Beaton <beatonma@gmail.com>
2019-12-03 21:15:02 +0000

    Created explicit header file with function prototypes because compiler on ubuntu was not generating them as expected.

---
+++ WS2812B/WS2812B.h
@@ -0,0 +1,16 @@
+void setupInputHandlers();
+void updateInputHandlers();
+
+void nextPattern();
+void nextMonochromePattern();
+void nextStaticColor();
+void nextMode();
+
+void rainbow();
+void rainbowWithGlitter();
+void addGlitter(fract8 chanceOfGlitter);
+void confetti();
+void sinelon();
+void bpm();
+void staticColor();
+void juggle();
