b762d379ed144354ab0a83178f3380c62a6e3342
Michael Beaton <beatonma@gmail.com>
2019-12-08 01:25:44 +0000

    Added 'animation' prefix to all animation functions. Added prototype for BrightnessPotentiometerHandler and new functions fillStaticColor, getCurrentColor and blendColors

--- WS2812B/WS2812B.h
+++ WS2812B/WS2812B.h
@@ -1,6 +1,7 @@
 #ifndef WS2812B_H
 #define WS2812B_H
 #include "input-buttons.cpp"
+#include "input-potentiometer.cpp"
 
 enum Mode: byte {
   Static = 0,              // All lights same color, no animation
@@ -16,21 +17,28 @@ void nextMonochromePattern();
 void nextStaticColor();
 void nextMode();
 
-void rainbow();
-void rainbowWithGlitter();
-void addGlitter(fract8 chanceOfGlitter);
-void confetti();
-void sinelon();
-void bpm();
-void staticColor();
-void juggle();
+// Animations with any color
+void animatePowerOn();
+void animationAddGlitter(fract8 chanceOfGlitter);
+void animationBpm();
+void animationConfetti();
+void animationJuggle();
+void animationRainbow();
+void animationRainbowWithGlitter();
+void animationSinelon();
+
+// Animatinos that use a single color
+void monochromeJuggle();
+
+// Solid, non animated color - all lights are the same
+void fillStaticColor();
+CRGB getCurrentColor();
+void blendColors(CRGB currentColor, CRGB nextColor, int speed = 30);
 
 class ModeButtonHandler: public AbstractButtonHandler {
   public:
   ModeButtonHandler(byte pin): AbstractButtonHandler(pin) {}
   void onButtonPressed();
-  void onLongPress();
-  void onLongPressHeld();
 };
 
 class OptionButtonHandler: public AbstractButtonHandler {
@@ -38,6 +46,12 @@ class OptionButtonHandler: public AbstractButtonHandler {
   OptionButtonHandler(byte pin): AbstractButtonHandler(pin) {}
   void onButtonPressed();
   void onLongPress();
+};
+
+class BrightnessPotentiometerHandler: public AbstractPotentiometerHandler {
+  public:
+  BrightnessPotentiometerHandler(byte pin): AbstractPotentiometerHandler(pin) {}
+  void onValueChanged(int value);
 };
 
 #endif
