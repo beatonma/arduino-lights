0186d525398b6802011dfb9c2c5db322cf14620b
Michael Beaton <beatonma@gmail.com>
2019-12-11 23:01:10 +0000

    Implemented simple noise tolerance when using analogRead. Small fluctuations are now ignored and do not trigger onValueChanged events.

--- WS2812B/input-potentiometer.cpp
+++ WS2812B/input-potentiometer.cpp
@@ -1,8 +1,10 @@
 #include "input.h"
 #include <Arduino.h>
+#include <stdlib.h>
 
 class AbstractPotentiometerHandler: AbstractInputHandler
 {
+  static const int ANALOG_READ_TOLERANCE = 5;  // Ignore changes in value smaller than this
 
 public:
   // Minimum change between previous_value_ and current_value_ that is considered
@@ -23,16 +25,18 @@ public:
   void update(void)
   {
     current_value_ = analogRead(pin_);
-    if (current_value_ != previous_value_) {
-      // TODO calculate running average to ignore signal noise
+    value_difference_ = abs(previous_value_ - current_value_);
+
+    if (value_difference_ > ANALOG_READ_TOLERANCE) {
       onValueChanged(current_value_);
+      previous_value_ = current_value_;
     }
-    previous_value_ = current_value_;
   }
 
 protected:
   int previous_value_ = 512;
   int current_value_ = 512;
+  int value_difference_ = 0;
 
   unsigned long action_started_timestamp_ = 0;
   unsigned long current_timestamp_ = 0;
