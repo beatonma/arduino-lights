bf394d06f24918a67cea26be1c4a3322ce44c72c
Michael Beaton <beatonma@gmail.com>
2019-12-14 18:56:16 +0000

    Implemented paletteGlitter, renamed paletteAnimation to paletteFlow

--- WS2812B/palette-animations.cpp
+++ WS2812B/palette-animations.cpp
@@ -7,9 +7,16 @@ namespace animations {
 
 CRGBPalette16 palette_;
 
-void paletteAnimation(CRGB leds[]) {
+void paletteFlow(CRGB leds[]) {
     FADE(20);
     fill_palette(leds, NUM_LEDS, hue_, 15, palette_, 240, LINEARBLEND);
+}
+
+void paletteGlitter(CRGB leds[]) {
+    FADE(3);
+    if (random8() < CHANCE_OF_GLITTER) {
+        leds[random16(NUM_LEDS)] += ColorFromPalette(palette_, random8());
+    }
 }
 
 }
