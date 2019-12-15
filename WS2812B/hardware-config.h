6d38272ec7405a467d5e73c6f2530c2c5ac07498
Michael Beaton <beatonma@gmail.com>
2019-12-15 21:52:39 +0000

    Update LED_TYPE to match the ones I am using. Improved docs for doxygen

--- WS2812B/hardware-config.h
+++ WS2812B/hardware-config.h
@@ -1,17 +1,27 @@
+/** @file
+ * Hardware configuration
+ */
 #ifndef HARDWARE_CONFIG_H
 #define HARDWARE_CONFIG_H
 #include <FastLED.h>
 
-// Hardware configuration
-
 FASTLED_USING_NAMESPACE
-#define LED_TYPE WS2811
+#define LED_TYPE WS2812B
 #define COLOR_ORDER GRB
 
-// Available values for temperature/correction can be found at
-// https://github.com/FastLED/FastLED/blob/master/color.h
+/**
+ * Available values for temperature/correction can be found at
+ * https://github.com/FastLED/FastLED/blob/master/color.h
+ */
 #define COLOR_CORRECTION UncorrectedColor
+
+/**
+ * Available values for temperature/correction can be found at
+ * https://github.com/FastLED/FastLED/blob/master/color.h
+ */
 #define COLOR_TEMPERATURE UncorrectedTemperature
+// #define COLOR_CORRECTION TypicalPixelString
+// #define COLOR_TEMPERATURE ClearBlueSky
 FASTLED_NAMESPACE_END
 
 #define SERIAL_BAUD_RATE 115200
@@ -23,7 +33,9 @@ FASTLED_NAMESPACE_END
 #define MODE_BUTTON_PIN 9
 #define OPTION_BUTTON_PIN 8
 
-#define MAX_BRIGHTNESS 255
+#define MAX_BRIGHTNESS 245
 #define MIN_BRIGHTNESS 5
+
+#define ARDUINO
 
 #endif
