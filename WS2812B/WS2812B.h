622658ccfc19ab50e5a1e55293f9f20d5d9a904b
Michael Beaton <beatonma@gmail.com>
2019-12-15 21:55:28 +0000

    Improved documentation for doxygen. Implemented changing color temperature by turning pot while holding Mode button. Implemented Auto 'mode' which is toggled by long-pressing Mode button. Improved input event handling to avoid interference between gestures - button actions can now be consumed to prevent unwanted callbacks.

--- WS2812B/WS2812B.h
+++ WS2812B/WS2812B.h
@@ -1,16 +1,40 @@
+/** @file
+ * Animation controller for WS2812B addressable LEDs.
+ *
+ * Based on FastLED "100-lines-of-code" demo reel by Mark Kriegsman, December 2014:
+ * https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
+ *
+ *
+ * Edited/extended by Michael Beaton, December 2019:
+ * - Implemented modes:
+ *   - ::Static
+ *   - ::MonochromeAnimated
+ *   - ::PaletteAnimated
+ *   - ::Animated
+ *   - ::Auto
+ * - Accepts input from potentiometer to change brightness.
+ * - Accepts input from 2 buttons to change modes and choose colors/animations.
+ * - Buttons and potentiometer can be combined to change animation speed and color temperature.
+ */
 #ifndef WS2812B_H
 #define WS2812B_H
 #include <stdint.h>
 #include "input-buttons.cpp"
 #include "input-potentiometer.cpp"
 
+/**
+ * Must be equal to number of values in ::Mode
+ */
 #define NUM_MODES 4
 enum Mode: uint8_t {
-  Static = 0,              // All lights same color, no animation
-  MonochromeAnimated = 1,  // All lights same base hue with animation
-  PaletteAnimated = 2,     // Same as MonochromeAnimated but using predefined sets of colors
-  Animated = 3,            // Animations with any color
+  Static = 0,              ///< All lights same color, no animation
+  MonochromeAnimated = 1,  ///< All lights same base hue with animation
+  PaletteAnimated = 2,     ///< Same as MonochromeAnimated but using predefined sets of colors
+  Animated = 3,            ///< Animations with any color,
+  // Auto = 4,                ///< Periodically change between any of the above, with any set of options.
 };
+
+
 
 void setupInputHandlers(void);
 void updateInputHandlers(void);
@@ -19,6 +43,7 @@ void nextPattern(void);
 void nextMonochromePattern(void);
 void nextStaticColor(void);
 void nextMode(void);
+void nextAuto(void);      ///< Randomly choose a new mode, color, pattern, or animation type.
 
 // Animations with any color
 void animatePowerOn(void);
@@ -32,6 +57,7 @@ class ModeButtonHandler: public AbstractButtonHandler {
   public:
   ModeButtonHandler(uint8_t pin): AbstractButtonHandler(pin) {}
   void onButtonPressed(void);
+  void onLongPress(void);
 };
 
 class OptionButtonHandler: public AbstractButtonHandler {
@@ -45,9 +71,9 @@ class BrightnessPotentiometerHandler: public AbstractPotentiometerHandler {
   public:
   BrightnessPotentiometerHandler(uint8_t pin): AbstractPotentiometerHandler(pin) {}
   void onValueChanged(int value);
-  void onValueChangedNoModifier(int value);       // Called when the pot is turned with no other inputs
-  void onValueChangedWithModeButton(int value);   // Called when the pot is turned while the Mode button is held down
-  // void onValueChangedWithOptionButton(int value); // Called when the pot is turned while the Option button is held down
+  void onValueChangedNoModifier(int value);       ///< Called when the pot is turned with no other inputs
+  void onValueChangedWithModeButton(int value);   ///< Called when the pot is turned while the Mode button is held down
+  void onValueChangedWithOptionButton(int value); ///< Called when the pot is turned while the Option button is held down
 };
 
 #endif
--- WS2812B/WS2812B.ino
+++ WS2812B/WS2812B.ino
@@ -1,3 +1,26 @@
+/** @file
+ * Animation controller for WS2812B addressable LEDs.
+ *
+ * Based on FastLED "100-lines-of-code" demo reel by Mark Kriegsman, December 2014:
+ * https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
+ *
+ *
+ * Edited/extended by Michael Beaton, December 2019:
+ * - Implemented modes:
+ *   - ::Static
+ *   - ::MonochromeAnimated
+ *   - ::PaletteAnimated
+ *   - ::Animated
+ *   - ::Auto
+ * - Implemented hardware controls:
+ *  - Change brightness by turning pot: BrightnessPotentiometerHandler::onValueChangedNoModifier
+ *  - Change mode by pressing Mode button: ModeButtonHandler::onButtonPressed
+ *  - Change color or animation (depending on current ::Mode) by pressing Option button: OptionButtonHandler::onButtonPressed
+ *  - Change animation speed by holding Mode button while turning pot: BrightnessPotentiometerHandler::onValueChangedWithModeButton
+ *  - Change color temperature by holding Option button while turning pot: BrightnessPotentiometerHandler::onValueChangedWithOptionButton
+ *  - Some modes also allow control of a second parameter by long-pressing the Option button: OptionButtonHandler::onLongPress
+ *  - Change color temperature by holding Option button while turning pot: BrightnessPotentiometerHandler::onValueChangedWithOptionButton
+ */
 #define DEBUG
 #include "debug.h"
 
@@ -11,28 +34,11 @@
 #include "WS2812B.h"
 FASTLED_USING_NAMESPACE
 
-
-//
-// Based on FastLED "100-lines-of-code" demo reel by Mark Kriegsman, December 2014:
-// https://github.com/FastLED/FastLED/blob/master/examples/DemoReel100/DemoReel100.ino
-//
-//
-// Edited/extended by Michael Beaton, December 2019
-// - Implemented modes:
-//   - Static: stay on a single color
-//   - MonochromeAnimated: enable animations but only using the color chosen in
-//   Static
-//   - Animated: use the animations from the example
-// - Accepts input from potentiometer to change brightness.
-// - Accepts input from 2 buttons to change modes and choose colors/animations.
-//
-
 #if defined(FASTLED_VERSION) && (FASTLED_VERSION < 3001000)
 #warning "Requires FastLED 3.1 or later; check github for latest code."
 #endif
 
 #define FRAMES_PER_SECOND_DEFAULT 120
-#define PALETTEOF(A) CRGBPalette16(A, A, A, A, A, A, A, A, A, A, A, A, A, A, A, A)
 
 CRGB leds_[NUM_LEDS];
 
@@ -41,11 +47,22 @@ uint8_t frames_per_second_ = FRAMES_PER_SECOND_DEFAULT;
 uint8_t mode_ = Mode::Static;
 uint8_t brightness_ = MAX_BRIGHTNESS;
 
-uint8_t animation_index_ = 0;             // Index of current pattern.
-uint8_t monochrome_animation_index_ = 0;  // Index of current monochrome pattern.
-uint8_t static_color_index_ = 0;          // Index of current static color.
-uint8_t palette_index_ = 0;               // Index of current color palette.
-uint8_t palette_animation_index_ = 0;     // Index of current palette animation.
+/// Index of current pattern.
+uint8_t animation_index_ = 0;
+
+/// Index of current monochrome pattern.
+uint8_t monochrome_animation_index_ = 0;
+
+/// Index of current static color.
+uint8_t static_color_index_ = 0;
+
+/// Index of current color palette.
+uint8_t palette_index_ = 0;
+
+/// Index of current palette animation.
+uint8_t palette_animation_index_ = 0;
+
+bool auto_cycle_ = false; ///< If true, modes and colors will be changed automatically.
 
 // Input handlers
 ModeButtonHandler mode_button_handler_(MODE_BUTTON_PIN);
@@ -53,50 +70,90 @@ OptionButtonHandler option_button_handler_(OPTION_BUTTON_PIN);
 BrightnessPotentiometerHandler brightness_potentiometer_handler_(BRIGHTNESS_POT_PIN);
 
 typedef void (*AnimationList[])(CRGB* leds);
+/**
+ * Animations used when mode_ is ::Animated
+ */
 AnimationList full_color_animations_ = {
-    animations::animationRainbow,
-    animations::animationRainbowWithGlitter,
-    animations::animationConfetti,
-    animations::animationSinelon,
-    animations::animationJuggle,
-    animations::animationBpm,
+  // animations::polychromeSplash,
+  animations::polychromeRainbow,
+  animations::polychromeRainbowWithGlitter,
+  animations::polychromeConfetti,
+  animations::polychromeSinelon,
+  animations::polychromeJuggle,
+  animations::polychromeBpm,
+  animations::monochromeRainbow,
 };
+
+/**
+ * Animations used when mode_ is ::MonochromeAnimated
+ */
 AnimationList monochrome_animations_ = {
-    animations::monochromeGlitter,
-    animations::monochromeSinelon,
-    animations::monochromeJuggle,
-    animations::monochromePulse,
-    animations::monochromeRainbow,
+  animations::monochromeGlitter,
+  animations::monochromeSinelon,
+  animations::monochromeJuggle,
+  animations::monochromePulse,
 };
+
+/**
+ * Animations used when mode_ is ::PaletteAnimated
+ */
 AnimationList palette_animations_ = {
   animations::paletteFlow,
+  animations::paletteFlowWithGlitter,
   animations::paletteGlitter,
 };
 
-// Available color definitions can be found at
-// https://github.com/FastLED/FastLED/blob/master/pixeltypes.h
-const ColorCode colors_[] = {
-    ColorCode::Purple,
-    ColorCode::HotPink,
-    ColorCode::FairyLightNCC,
-    ColorCode::White,
-    ColorCode::OrangeRed,
-    ColorCode::Red,
-    ColorCode::Yellow,
-    ColorCode::Green,
-    ColorCode::Cyan,
-    ColorCode::Blue,
+/**
+ * Available color definitions can be found at
+ * https://github.com/FastLED/FastLED/blob/master/pixeltypes.h
+ */
+const uint32_t colors_[] = {
+  ColorCode::Purple,
+  ColorCode::Magenta,
+  ColorCode::HotPink,
+  ColorCode::FairyLightNCC,
+  ColorCode::White,
+  ColorCode::OrangeRed,
+  ColorCode::Red,
+  ColorCode::Yellow,
+  ColorCode::Green,
+  ColorCode::Cyan,
+  ColorCode::Blue,
 };
 
+/**
+ * Themed collections of colors used in ::PaletteAnimated.
+ */
 CRGBPalette16 palettes_[] = {
-  unicornPalette(),
+  palettes::unicornPalette(),
+  PartyColors_p,
+  palettes::summerPalette(),
   CloudColors_p,
   OceanColors_p,
   ForestColors_p,
-  PartyColors_p,
   LavaColors_p,
 };
 
+/**
+ * LED color temperature may be adjusted to any of these values.
+ */
+const uint32_t temperatures_[] = {
+  ColorTemperature::Candle,
+  ColorTemperature::Tungsten40W,
+  ColorTemperature::Tungsten100W,
+  ColorTemperature::Halogen,
+  ColorTemperature::CarbonArc,
+  ColorTemperature::HighNoonSun,
+  ColorTemperature::DirectSunlight,
+  ColorTemperature::OvercastSky,
+  ColorTemperature::ClearBlueSky,
+  ColorTemperature::UncorrectedTemperature,
+};
+
+
+/**
+ * Arduino setup runs when first powered on.
+ */
 void setup(void) {
   #ifdef DEBUG
   Serial.begin(SERIAL_BAUD_RATE);
@@ -116,16 +173,25 @@ void setup(void) {
   PRINTLN("OK GO");
 }
 
+/**
+ * Arduino loop runs repeatedly after setup() completes.
+ */
 void loop(void) {
   updateInputHandlers();
   EVERY_N_MILLISECONDS(20) {
     animations::hue_++;
   }
 
+  // if (mode_ == Mode::Auto) {
+  if (auto_cycle_) {
+    EVERY_N_SECONDS(30) {
+      nextAuto();
+    }
+  }
+
   switch (mode_) {
     case Mode::Static:
       animations::transitionLinearToSolid(leds_, getCurrentColor());
-
       break;
     case Mode::MonochromeAnimated:
       monochrome_animations_[monochrome_animation_index_](leds_);
@@ -187,20 +253,53 @@ void nextMode(void) {
   mode_ = (mode_ + 1) % NUM_MODES;
 }
 
-void fillStaticColor(void) {
-  fill_solid(leds_, NUM_LEDS, getCurrentColor());
+/**
+ * Randomly choose a new ::Mode, animation or color.
+ */
+void nextAuto(void) {
+  const uint8_t result = random8();
+  if (result < 85) {
+    // Change the mode.
+    nextMode();
+  }
+  else if (result < 170) {
+    // Change the animation.
+    nextPattern();
+    nextMonochromePattern();
+    nextPaletteAnimation();
+  }
+  else {
+    // Change the color.
+    nextPalette();
+    nextStaticColor();
+  }
 }
 
 CRGB getCurrentColor(void) {
   return toCRGB(colors_[static_color_index_]);
 }
 
+/**
+ * Cycle through ::Mode%s
+ */
 void ModeButtonHandler::onButtonPressed(void) {
   nextMode();
   PRINT("nextMode:");
   PRINTLN(mode_);
 }
 
+/**
+ * Toggle automatic selection of colors.
+ */
+void ModeButtonHandler::onLongPress(void) {
+  auto_cycle_ = !auto_cycle_;
+  PRINT("auto: ");
+  PRINTLN(auto_cycle_);
+}
+
+/**
+ * Cycle through main color/animation options.
+ */
 void OptionButtonHandler::onButtonPressed(void) {
   switch (mode_) {
     case Mode::Animated:
@@ -215,8 +314,10 @@ void OptionButtonHandler::onButtonPressed(void) {
       break;
     case Mode::Static:
       nextStaticColor();
-      PRINT("nextStaticColor:");
-      PRINTLN(static_color_index_);
+      PRINT("nextStaticColor: ");
+      PRINT(static_color_index_);
+      PRINT(" ");
+      PRINTLN(colors_[static_color_index_]);
       break;
     case Mode::PaletteAnimated:
       nextPalette();
@@ -226,6 +327,9 @@ void OptionButtonHandler::onButtonPressed(void) {
   }
 }
 
+/**
+ * Cycle through additional animation/color options.
+ */
 void OptionButtonHandler::onLongPress(void) {
   switch (mode_) {
     case Mode::MonochromeAnimated:
@@ -241,11 +345,17 @@ void BrightnessPotentiometerHandler::onValueChanged(const int value) {
   if (mode_button_handler_.isDown()) {
     onValueChangedWithModeButton(value);
   }
+  else if (option_button_handler_.isDown()) {
+    onValueChangedWithOptionButton(value);
+  }
   else {
     onValueChangedNoModifier(value);
   }
 }
 
+/**
+ * Change LED brightness based on pot position.
+ */
 void BrightnessPotentiometerHandler::onValueChangedNoModifier(const int value) {
   // Handle fuzziness at extreme positions to avoid flickering.
   if (value < 20) {
@@ -257,8 +367,12 @@ void BrightnessPotentiometerHandler::onValueChangedNoModifier(const int value) {
   }
 }
 
+/**
+ * Change animation speed by turning pot while Mode button is held.
+ */
 void BrightnessPotentiometerHandler::onValueChangedWithModeButton(const int value) {
-  // Change animation speed by holding Mode button while turning the brightness pot.
+  mode_button_handler_.consumeAction();  // Cancel any further callbacks from the button.
+
   animations::animation_speed_multiplier_ = (float) map(value, 0, 1023, 1, 20) / 10.f;
   frames_per_second_ = animations::animation_speed_multiplier_ * FRAMES_PER_SECOND_DEFAULT;
 
@@ -266,6 +380,26 @@ void BrightnessPotentiometerHandler::onValueChangedWithModeButton(const int valu
   PRINT("x -> ");
   PRINT(frames_per_second_);
   PRINTLN("fps");
+}
+
+/**
+ * Change LED color temperature by turning pot while Option button is held.
+ */
+void BrightnessPotentiometerHandler::onValueChangedWithOptionButton(const int value) {
+  option_button_handler_.consumeAction();  // Cancel any further callbacks from the button.
+
+  // Change color temperature by holding Option button while turning the brightness pot.
+  uint32_t temperature;
+  if (value < 20) {
+    temperature = temperatures_[0];
+  }
+  else if (value > 1010) {
+    temperature = temperatures_[ARRAY_SIZE(temperatures_) - 1];
+  }
+  else {
+    temperature = temperatures_[map(value, 20, 1010, 0, ARRAY_SIZE(temperatures_) - 1)];
+  }
+  FastLED.setTemperature(CRGB(temperature));
 }
 
 FASTLED_NAMESPACE_END
